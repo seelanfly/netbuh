553397fa986d30ee4ac927a558ab34fef93bfb34
Dhanaseelan Arydoss <seelanfly@outlook.com>
2026-02-22 11:46:06 -0500

    Add tomcat app

---
+++ src/main/java/com/webbynova/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.setContentType("text/plain");
+        resp.getWriter().write("Hello from Java Servlet running in Tomcat!");
+    }
+}
+
